bb22d13b6052de2695ebf916f156b90f5ac52067
nhemgomia@gmail.com <gomianehemiahrenel@gmail.com>
2023-07-30 00:18:31 +0800

    Implement listing Map

--- components/modals/rent-modal.tsx
+++ components/modals/rent-modal.tsx
@@ -9,6 +9,8 @@ import Modal from "./modal";
 import Heading from "../heading";
 import { categories } from "../navbar/categories";
 import CategoryInput from "../inputs/category-input";
+import CountrySelect from "../inputs/country-select";
+import dynamic from "next/dynamic";
 
 enum STEPS {
   CATEGORY = 0,
@@ -48,6 +50,11 @@ const RentModal = () => {
   });
 
   const category = watch('category');
+  const location = watch('location');
+
+  const Map = useMemo(() => dynamic(() => import ('../map'), {
+    ssr: false
+  }), [location])
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -106,11 +113,29 @@ const RentModal = () => {
     </div>
   )
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className="flex flex-col gap-8">
+        <Heading 
+          title="Where is your place located?"
+          subtitle="Help guess find you!"
+        />
+        <CountrySelect
+          value={location}
+          onChange={(value) => setCustomValue('location', value)}
+        />
+        <Map 
+          center={location?.latlng}
+        />
+      </div>
+    )
+  }
+
   return ( 
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
       secondaryAction={step === STEPS.CATEGORY ? undefined : onBack}
