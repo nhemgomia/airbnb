0695d76b6b71df492aac33abf293224e8153adaa
nhemgomia@gmail.com <gomianehemiahrenel@gmail.com>
2023-07-31 03:56:01 +0800

    Implement listing-card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,46 @@
-export default function Home() {
+import getCurrentUser from "@/actions/get-current-user";
+import getListings from "@/actions/get-listings";
+
+import ClientOnly from "@/components/client-only";
+import Container from "@/components/container";
+import EmptyState from "@/components/empty-state";
+import ListingCard from "@/components/listings/listing-card";
+
+export default async function Home() {
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if (listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset />
+      </ClientOnly>
+    )
+  }
+
   return (
-    <div></div>
+    <ClientOnly>
+      <Container>
+        <div className="
+          pt-24
+          grid
+          grid-cols-1
+          sm:grid-cols-2
+          md:grid-cols-3
+          lg:grid-cols-4
+          xl:grid-cols-5
+          2xl:grid-cols-6
+          gap-8
+        ">
+          {listings.map((listing: any) => (
+            <ListingCard
+              currentUser={currentUser}
+              key={listing.id}
+              data={listing}
+            />
+          ))}
+        </div>
+      </Container>
+    </ClientOnly>
   );
 }
