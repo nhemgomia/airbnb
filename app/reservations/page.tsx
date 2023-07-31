c4fdd08b1a35f00687c188b015de5d91addda904
nhemgomia@gmail.com <gomianehemiahrenel@gmail.com>
2023-08-01 06:02:40 +0800

    Implement reservations page

---
+++ app/reservations/page.tsx
@@ -0,0 +1,44 @@
+import getCurrentUser from "@/actions/get-current-user";
+import getReservations from "@/actions/get-reservations";
+import ClientOnly from "@/components/client-only";
+import EmptyState from "@/components/empty-state";
+import ReservationsClient from "./reservations-client";
+
+const ReservationsPage = async () => {
+  const currentUser = await getCurrentUser();
+  
+  if (!currentUser) 
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="Unauthorized"
+          subtitle="Please login"
+        />
+      </ClientOnly>
+    );
+  
+  const reservations = await getReservations({
+    authorId: currentUser.id
+  });
+
+  if (reservations.length === 0)
+    return (
+      <ClientOnly>
+        <EmptyState
+          title="No reservations found"
+          subtitle="Looks like you have no reservations on your properties"
+        />
+      </ClientOnly>
+    )
+  
+  return (
+    <ClientOnly>
+      <ReservationsClient
+        reservations={reservations}
+        currentUser={currentUser}
+      />
+    </ClientOnly>
+  )
+};
+
+export default ReservationsPage;
